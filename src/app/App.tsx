c2a7c2fc441dbc803984af73b1d36b515356c26a
Asel <asel.alimbaeva.kk@gmail.com>
2024-07-11 14:08:07 +0300

    feat: add Router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Search from './components/Search';
 import '../app/styles/style.css';
+import Page404 from './page/Page404';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <Search />
-    </div>
+    <Router>
+      <main>
+        <Routes>
+          <Route path="/" element={<Search />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </main>
+    </Router>
   );
 };
 
