d19f1960436f3c98f4debd0843f9497192aca518
Asel <asel.alimbaeva.kk@gmail.com>
2024-07-11 08:14:04 +0300

    refactor: changed to functional components Results class

--- src/app/components/Results.tsx
+++ src/app/components/Results.tsx
@@ -1,31 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ResultsProps } from '../types/interface';
 
-class Results extends Component<ResultsProps> {
-  render(): React.ReactNode {
-    const { data } = this.props;
-
-    if (!data.length) {
-      return (
-        <h2 className="emty-message">
-          Unfortunately, there is no data available for your request.
-        </h2>
-      );
-    }
-
+const Results: React.FC<ResultsProps> = ({ data }) => {
+  if (!data.length) {
     return (
-      <div className="search-block">
-        <ul className="answer-block">
-          {data.map((animal) => (
-            <li key={animal.uid} className="answer-item">
-              <p>{animal.name}</p>
-              <p>Earth Animal: {animal.earthAnimal ? 'Yes' : 'No'}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <h2 className="emty-message">
+        Unfortunately, there is no data available for your request.
+      </h2>
     );
   }
-}
+
+  return (
+    <div className="search-block">
+      <ul className="answer-block">
+        {data.map((animal) => (
+          <li key={animal.uid} className="answer-item">
+            <p>{animal.name}</p>
+            <p>Earth Animal: {animal.earthAnimal ? 'Yes' : 'No'}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Results;
