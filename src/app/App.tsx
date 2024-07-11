9c9e89ae46d5e041b0fbc4760ae092990bef07d2
Asel <asel.alimbaeva.kk@gmail.com>
2024-07-11 11:45:41 +0300

    refactor: changed to functional components App class

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Search from './components/Search';
 import '../app/styles/style.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        {' '}
-        <Search />{' '}
-      </div>
-    );
-  }
-}
+const App: React.FC = () => {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+};
 
 export default App;
