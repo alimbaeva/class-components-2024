8595c5215a28e5b4e88a910f61b4b527c5402c2c
Asel <asel.alimbaeva.kk@gmail.com>
2024-07-11 08:09:16 +0300

    fix: lint

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -1,23 +1,21 @@
-import React, { Component, ReactNode } from 'react';
+import React from 'react';
 import '../styles/loadung.css';
 
-class Loading extends Component {
-  render(): ReactNode {
-    return (
-      <section className="loader">
-        <div className="loader-inner-block">
-          <div className="loader-block">
-            <div className="item1"></div>
-            <div className="item2"></div>
-            <div className="item3"></div>
-            <div className="item4"></div>
-            <div className="item5"></div>
-            <div className="item6"></div>
-          </div>
+const Loading = () => {
+  return (
+    <section className="loader">
+      <div className="loader-inner-block">
+        <div className="loader-block">
+          <div className="item1"></div>
+          <div className="item2"></div>
+          <div className="item3"></div>
+          <div className="item4"></div>
+          <div className="item5"></div>
+          <div className="item6"></div>
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
 
 export default Loading;
